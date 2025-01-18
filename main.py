0c6c89417f90e37d8cbd55b350b068626f13938d
Mohammad <mhamdl2amen@gmail.com>
2025-01-18 18:43:48 +0200

    split the chart bar between men and women by category

--- main.py
+++ main.py
@@ -50,4 +50,21 @@ v_bar.update_layout(
     yaxis_title='Number of prizes'
 )
 v_bar.write_image('images/num_prizes_awarded.png')
-v_bar.show()
+#first prize in the field of economics awarded
+first_econ = df[df['category']=='Economics']['year'].min()
+name_first_econ = df[df['year']==first_econ]['full_name'].values[0]
+print(f"First prize in the field of economics awarded is:{name_first_econ}")
+#create a chart bar that shows the split between men and women by category.
+category_men_women=df.groupby(['category','sex'],as_index=False).agg({'prize':pd.Series.count})
+#we need to sort the value to put the value of women on top of bar chart because it only 6.5%
+category_men_women.sort_values('prize',ascending=False,inplace=True)
+v_bar_split=px.bar(x=category_men_women.category,
+                   y=category_men_women.prize,
+                   color=category_men_women.sex,
+                   title="Number Of Prizes Awarded Per Caregory Split")
+v_bar_split.update_layout(xaxis_title='Category'
+                          ,yaxis_title='Number of Prizes')
+v_bar_split.write_image('images/num_prizes_awarded_split.png')
+v_bar_split.show()
+
+
